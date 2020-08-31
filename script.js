7f7f7b2e70b0309a09558189a8bbd0366760fd3a
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-08-31 13:48:10 -0500

    added red background and yellow/remove buttons upon generating

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 $( document ).ready( onReady );
 
+//Global Variables
+let incrementing = 1;
+incrementing++;
+
 function onReady(){
     $( document ).on( 'click', '.generateBtn', generate );
+    $( document ).on( 'click', 'yellowBtn', backgroundYellow );
 } // end onReady
 
 function generate(){
-    
+    console.log( 'in generate' );
+    $( '.block' ).css( 'background-color', 'red' );
+    $( '.block' ).append(`
+        <p></p>
+        <button class="yellowBtn">Yellow</button>
+        <button class="removeBtn">Remove</button>
+    `)
+
+    incrementing();
 } // end generate
+
+function backgroundYellow(){
+    $( '.block' ).css( 'background-color', 'yellow' );
+} // end backgroundYellow

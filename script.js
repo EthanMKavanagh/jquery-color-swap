3520c9a67124a11967ac9f030ce2b51a59ec8fd4
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-08-31 14:31:57 -0500

    added comments, and tried to get it working

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ function onReady(){
     $( document ).on( 'click', '.removeBtn', removeDiv );
 } // end onReady
 
+// generate the divs
 function generate(){
     console.log( 'in generate' );
+    // append div with p tag and buttons
     $( '.block' ).append(`
         <div class="div">
             <p class="count"></p>
@@ -15,23 +17,26 @@ function generate(){
             <button class="removeBtn">Remove</button>
         </div>
     `)
+    // background color red
     $( '.div' ).css( 'background-color', 'red' );
 
+    // call incrementing function
     incrementing();
 } // end generate
 
-
+// yellow button background color switch
 function backgroundYellow(){
     $( '.div' ).css( 'background-color', 'yellow' );
 } // end backgroundYellow
 
-
+// displays count
 let number = 0;
 function incrementing(){
     number++;
     $( '.count' ).append( number )
 } // end incrementing
 
+// remove div
 function removeDiv(){
     $( '.div' ).remove();
 } // end removeDiv
